625eefbd1c3c1825b072b333c47ba51cf1ee570a
Renan Stein <132713100+RenanStein@users.noreply.github.com>
2026-06-25 21:08:39 -0300

    .
    .

--- js/formulario_api/formulario_api.js
+++ js/formulario_api/formulario_api.js
@@ -6,5 +6,36 @@ export default function initFormularioApi(){
 const button = document.querySelector('[data-button]');
 const nomeInput = document.querySelector('[data-nome="input"]');
 const cepInput = document.querySelector('[data-cep="input"]');
-console.log(cepInput)
+const nomeOutput = document.querySelector('[data-nome="output"]');
+const cepOutput = document.querySelector('[data-cep="output"]');
+const logradouro = document.querySelector('[data-logradouro]');
+const bairro = document.querySelector('[data-bairro]');
+const cidade = document.querySelector('[data-cidade]');
+const estado = document.querySelector('[data-estado]');
+
+let dados = [];
+
+function enviarFormulario(){
+  const dadosFormulario = new objetoDados(nomeInput.value, cepInput.value);
+  dados.push(dadosFormulario);
+
+  let cepFormatado = cepInput.value;
+
+  fetch('https://viacep.com.br/ws/' + cepFormatado + '/json/')
+  .then(response => {
+    response.json().then(j => {
+      console.log(j)
+      console.log(j.estado)
+    })
+  })
+}
+
+
+function objetoDados(nome, cep){
+  this.nome = nome;
+  this.cep = cep;
+}
+
+button.addEventListener('click', enviarFormulario);
+console.log(nomeInput)
 
